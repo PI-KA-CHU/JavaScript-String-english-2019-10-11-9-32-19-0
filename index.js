64c4e96388dcb0e07fbd88db07fd4184b34644b6
coolPig <529709737@qq.com>
2020-06-20 23:55:13 +0800

    JS编程

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
+console.log(name.toUpper());
 
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
+//TODO write your code here..
+function firstUpperCase(str) {
+  return str.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase());
+}
+var str = firstUpperCase(sentence);
+console.log(str)
 
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+console.log(money.substring(1));
